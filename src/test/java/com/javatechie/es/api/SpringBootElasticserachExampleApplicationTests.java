8d38776ec92104bf343abafb7d008a7d1a5495f2
najeebshaik08@gmail.com <najeebshaik08@gmail.com>
2024-05-16 21:03:17 +0800

    testing

--- src/test/java/com/javatechie/es/api/SpringBootElasticserachExampleApplicationTests.java
+++ src/test/java/com/javatechie/es/api/SpringBootElasticserachExampleApplicationTests.java
@@ -1,16 +1,37 @@
-//package com.javatechie.es.api;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class SpringBootElasticserachExampleApplicationTests {
-//
-//	@Test
-//	public void contextLoads() {
-//	}
-//
-//}
+package com.javatechie.es.api;
+
+import static org.mockito.BDDMockito.verify;
+import static org.mockito.BDDMockito.given;
+import static org.assertj.core.api.Assertions.assertThat;
+ 
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import com.javatechie.es.api.model.User;
+import com.javatechie.es.api.repository.UserRepository;
+ 
+@ExtendWith(MockitoExtension.class)
+ 
+public class SpringBootElasticserachExampleApplicationTests {
+
+	
+	@Mock
+	UserRepository userRepository;
+	
+	@Test
+	public void findByFirstName() {
+		
+		given(userRepository.findByUsername("")).willReturn(new User());
+	}
+	@Test
+	public void login() {
+		
+		given(userRepository.findByUsername("")).willReturn(new User());
+	}
+
+}
